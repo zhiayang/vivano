d94be97b48712900a8fea1657915e3834c154a57
zhiayang <gpg@tsumaran.ai>
2022-08-13 18:48:32 +0800

    more cleanup

--- source/main.cpp
+++ source/main.cpp
@@ -19,7 +19,72 @@
 #include "vivado.h"
 #include "project.h"
 
+using zst::Result;
 static constexpr std::string_view VERSION = "0.1.0";
+
+static Result<void, std::string> run_subcommand(vvn::Project& project, std::string_view command, std::span<std::string_view> args)
+{
+	using namespace vvn;
+	if(command == vvn::CMD_CLEAN)
+	{
+		if(args::check(args, args::HELP))
+			help::showCleanHelp(), exit(0);
+
+		return project.clean(args);
+	}
+	else if(command == vvn::CMD_CREATE_IP)
+	{
+		if(args::check(args, args::HELP))
+			help::showCreateIpHelp(), exit(0);
+
+		return vvn::createIpWithVivadoGUI(project, args);
+	}
+	else if(command == vvn::CMD_BUILD)
+	{
+		if(args::check(args, args::HELP))
+			help::showBuildHelp(), exit(0);
+
+		auto vivado = project.launchVivado();
+		return project.setup(vivado).flatmap([&]() {
+			return project.buildAll(vivado, args);
+		});
+	}
+	else if(command == vvn::CMD_SYNTH)
+	{
+		if(args::check(args, args::HELP))
+			help::showSynthHelp(), exit(0);
+
+		auto vivado = project.launchVivado();
+		return project.setup(vivado).flatmap([&]() {
+			return project.synthesise(vivado, args).remove_value();
+		});
+	}
+	else if(command == vvn::CMD_IMPL)
+	{
+		if(args::check(args, args::HELP))
+			help::showImplHelp(), exit(0);
+
+		auto vivado = project.launchVivado();
+		return project.setup(vivado).flatmap([&]() {
+			return project.implement(vivado, args).remove_value();
+		});
+	}
+	else if(command == vvn::CMD_BITSREAM)
+	{
+		if(args::check(args, args::HELP))
+			help::showBitstreamHelp(), exit(0);
+
+		auto vivado = project.launchVivado();
+		return project.setup(vivado).flatmap([&]() {
+			return project.writeBitstream(vivado, args).remove_value();
+		});
+	}
+	else
+	{
+		vvn::help::showCommandList();
+		return ErrFmt("unsupported command '{}'", command);
+	}
+}
 
 
 int main(int argc, char** argv)
@@ -62,104 +127,19 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 
-	// if we are asking for help, don't bother to launch vivado
-	if(vvn::args::check(args, vvn::args::HELP))
-	{
-		using namespace vvn;
-		using namespace vvn::help;
-		// else if(command == CMD_BUILD)
-		// 	showBuildHelp();
-		// else if(command == CMD_SYNTH)
-		// 	showSynthHelp();
-		// else if(command == CMD_IMPL)
-		// 	showImplHelp();
-		// else if(command == CMD_BITSREAM)
-		// 	showBitstreamHelp();
-		// else if(command == CMD_CLEAN)
-		// 	showCleanHelp();
-
-		exit(0);
-	}
-
 	// try to read a vivano-project.json
 	auto config_or_err = vvn::parseProjectJson(vvn::PROJECT_JSON_FILENAME);
 	if(not config_or_err.ok())
 		vvn::error_and_exit("failed to read project json: {}", config_or_err.error());
 
 	auto project = vvn::Project(config_or_err.unwrap());
-	zst::Result<void, std::string> result {};
-
-	using namespace vvn;
-	if(command == vvn::CMD_CLEAN)
-	{
-		if(args::check(args, args::HELP))
-			help::showCleanHelp(), exit(0);
-
-		result = project.clean(args);
-	}
-	else if(command == vvn::CMD_CREATE_IP)
-	{
-		if(args::check(args, args::HELP))
-			help::showCreateIpHelp(), exit(0);
-
-		result = vvn::createIpWithVivadoGUI(project, args);
-	}
-	else if(command == vvn::CMD_BUILD)
-	{
-		if(args::check(args, args::HELP))
-			help::showBuildHelp(), exit(0);
-
-		auto vivado = project.launchVivado();
-		result = project.setup(vivado).flatmap([&]() {
-			return project.buildAll(vivado, args);
-		});
-	}
-	else if(command == vvn::CMD_SYNTH)
-	{
-		if(args::check(args, args::HELP))
-			help::showSynthHelp(), exit(0);
-
-		auto vivado = project.launchVivado();
-		result = project.setup(vivado).flatmap([&]() {
-			return project.synthesise(vivado, args).remove_value();
-		});
-	}
-	else if(command == vvn::CMD_IMPL)
-	{
-		if(args::check(args, args::HELP))
-			help::showImplHelp(), exit(0);
-
-		auto vivado = project.launchVivado();
-		result = project.setup(vivado).flatmap([&]() {
-			return project.implement(vivado, args).remove_value();
-		});
-	}
-	else if(command == vvn::CMD_BITSREAM)
-	{
-		if(args::check(args, args::HELP))
-			help::showBitstreamHelp(), exit(0);
-
-		auto vivado = project.launchVivado();
-		result = project.setup(vivado).flatmap([&]() {
-			return project.writeBitstream(vivado, args).remove_value();
-		});
-	}
-	else
-	{
-		// TODO: repl mode
-		zpr::println("unsupported command '{}'", command);
-		vvn::help::showCommandList();
-		exit(1);
-	}
-
+	auto result = run_subcommand(project, command, args);
 
 	if(result.is_err())
-	{
-		zpr::fprintln(stderr, "\nerror encountered: {}", result.error());
-		exit(1);
-	}
-}
+		zpr::fprintln(stderr, "\nerrors were encountered: {}", result.error());
 
+	exit(result.is_err() ? 1 : 0);
+}
 
 
 
